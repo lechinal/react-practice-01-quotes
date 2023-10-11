4f0d764a728e6c7883bf478c78a24b3b21a525fa
lechinal <alin.lechintan@outlook.com>
2023-10-11 22:30:06 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
+import React from 'react';
+import Quotes from './data/data';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      react-practice-01-quotes
+    <div className="App">
+      <h1>Aplicația cu Citate</h1>
+      {Quotes.map(quote => (
+        <div key={quote.id}>
+          <h2>{quote.author}</h2>
+          <p>{quote.quote}</p>
+          <img src={quote.authorImage} alt={quote.author} />
+        </div>
+      ))}
     </div>
   );
 };
---
+++ src/data/data.jsx
@@ -0,0 +1,152 @@
+const Quotes = [
+  {
+    id: 1,
+    author: 'Oscar Wide',
+    quote: 'Be yourself; everyone else is already taken.',
+    authorImage: '../src/images/Oscar-Wilde.jpg',
+  },
+
+  {
+    id: 2,
+    author: 'Marilyn Monroe',
+    quote:
+      "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+    authorImage: '',
+  },
+
+  {
+    id: 3,
+    author: 'Albert Einstein',
+    quote:
+      "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    authorImage: '',
+  },
+
+  {
+    id: 4,
+    author: 'Frank Zappa',
+    quote: 'So many books, so little time.',
+    authorImage: '',
+  },
+
+  {
+    id: 5,
+    author: 'Marcus Tullius Cicero',
+    quote: 'A room without books is like a body without a soul.',
+    authorImage: '',
+  },
+
+  {
+    id: 6,
+    author: 'Bernard M. Baruch',
+    quote:
+      "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+    authorImage: '',
+  },
+
+  {
+    id: 7,
+    author: 'Mahatma Gandhi',
+    quote: 'Be the change that you wish to see in the world.',
+    authorImage: '',
+  },
+
+  {
+    id: 8,
+    author: 'Oscar Wilde',
+    quote:
+      'To live is the rarest thing in the world. Most people exist, that is all.',
+    authorImage: '',
+  },
+
+  {
+    id: 9,
+    author: 'Oscar Wilde',
+    quote: 'Always forgive your enemies; nothing annoys them so much.',
+    authorImage: '',
+  },
+
+  {
+    id: 10,
+    author: 'Friedrich Nietzsche',
+    quote: 'Without music, life would be a mistake.',
+    authorImage: '',
+  },
+
+  {
+    id: 11,
+    author: 'Mahatma Gandhi',
+    quote:
+      'Live as if you were to die tomorrow. Learn as if you were to live forever.',
+    authorImage: '',
+  },
+
+  {
+    id: 12,
+    author: 'Steve Jobs',
+    quote:
+      "Here's to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They're not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do.",
+    authorImage: '',
+  },
+
+  {
+    id: 13,
+    author: 'Thomas A. Edison',
+    quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    authorImage: '',
+  },
+
+  {
+    id: 14,
+    author: 'Bob Marley',
+    quote: 'One good thing about music, when it hits you, you feel no pain.',
+    authorImage: '',
+  },
+
+  {
+    id: 15,
+    author: 'Bob Marley',
+    quote:
+      'The greatness of a man is not in how much wealth he acquires, but in his integrity and his ability to affect those around him positively.',
+    authorImage: '',
+  },
+
+  {
+    id: 16,
+    author: 'Bob Marley',
+    quote:
+      'The truth is, everyone is going to hurt you. You just got to find the ones worth suffering for.',
+    authorImage: '',
+  },
+
+  {
+    id: 17,
+    author: 'Socrates',
+    quote: 'The only true wisdom is in knowing you know nothing.',
+    authorImage: '',
+  },
+
+  {
+    id: 18,
+    author: 'Mahatma Gandhi',
+    quote: 'An eye for an eye will only make the whole world blind.',
+    authorImage: '',
+  },
+
+  {
+    id: 19,
+    author: 'Paulo Coelho',
+    quote: 'One is loved because one is loved. No reason is needed for loving.',
+    authorImage: '',
+  },
+
+  {
+    id: 20,
+    author: 'Leo Tolstoy',
+    quote:
+      'Everyone thinks of changing the world, but no one thinks of changing himself.',
+    authorImage: '',
+  },
+];
+
+export default Quotes;
