2139f696e2acb169177f2fb49c30d5ae2ac0f99c
lechinal <alin.lechintan@outlook.com>
2023-10-12 17:25:51 +0300

    update

--- src/components/QuotesTracker/QuotesTracker.jsx
+++ src/components/QuotesTracker/QuotesTracker.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './QuotesTracker.module.css';
 import QuotesObj from 'data/data';
 
@@ -6,7 +6,8 @@ function QuotesTracker({ readQuotes, remainingQuotes, percentageRead }) {
   return (
     <div className={styles.trackerBox}>
       <p>
-        {readQuotes} / {QuotesObj.length} quotes read ({percentageRead}%)
+        {readQuotes} / {QuotesObj.length} quotes read (
+        {percentageRead.toFixed(0)}%)
       </p>
       <p>{remainingQuotes} quotes left</p>
     </div>
